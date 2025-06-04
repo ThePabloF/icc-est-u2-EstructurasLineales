a8d9c90784cc85956c18a5e30e3ec5ca03c1c579
Pablo Feijo <pfeijo34@gmail.com>
2025-06-04 01:09:02 -0500

    first commit

---
+++ src/controllers/Stack.java
@@ -0,0 +1,51 @@
+package controllers;
+import java.util.EmptyStackException;
+
+import models.Node;
+
+public class Stack {
+    private Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    //Metodo que ingrese un valor int
+    public void push (int value){
+        Node newNode = new Node(value);
+        newNode.setNext(top);
+        top = newNode;
+
+    }
+
+    public int pop(){
+        if (isEmpty())
+            throw new EmptyStackException();
+        
+        int value = top.getValue();
+        top = top.getNext();
+
+        return value;
+       // return top.getNext().getValue();
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public int peek(){
+        if (isEmpty())
+            throw new EmptyStackException();
+        
+        return top.getValue();
+    }
+    public void printStack(){
+        Node current = top;
+        while (current!=null) {
+            System.out.print(current.getValue());
+            current = current.getNext();
+        }
+    }
+    
+
+}
